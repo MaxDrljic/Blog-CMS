5e50d6bf8d2e5ef9dc1ab28aa4bf7aefebc99da0
MaxDrljic <max_spree@hotmail.com>
2018-12-24 12:58:45 +0100

    Soft delete - move to trash

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -114,6 +114,12 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+
+        $post->delete();
+
+        toastr()->success('The post was moved to trash!');
+
+        return redirect()->back();
     }
 }

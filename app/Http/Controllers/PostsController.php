c0c850ac9f2222df18fcdc0ef2166405d11cdc3d
MaxDrljic <max_spree@hotmail.com>
2018-12-25 17:58:45 +0100

    Update posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Session;
 use App\Post;
 use App\Category;
-use Session;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -91,7 +91,9 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+
+        return view('admin.posts.edit')->with('post', $post)->with('categories', Category::all());
     }
 
     /**
@@ -103,7 +105,34 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title'         => 'required',
+            'content'       => 'required',
+            'category_id'   => 'required'
+        ]);
+
+        $post = Post::find($id);
+
+        if($request->hasFile('featured'))
+        {
+            $featured = $request->featured;
+
+            $featured_new_name = time() . $featured->getClientOriginalName();
+
+            $featured->move('uploads/posts', $featured_new_name);
+
+            $post->featured = 'uploads/posts/' . $featured_new_name;
+        }
+
+        $post->title = $request->title;
+        $post->content = $request->content;
+        $post->category_id = $request->category_id;
+
+        $post->save();
+
+        toastr()->success('Post updated successfully!');
+
+        return redirect()->route('posts');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,16 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
         'as'   => 'post.restore'
     ]);
 
+    Route::get('/posts/edit/{id}', [
+        'uses' => 'PostsController@edit',
+        'as'   => 'post.edit'
+    ]);
+
+    Route::post('/post/update/{id}', [
+        'uses' => 'PostsController@update',
+        'as'   => 'post.update'
+    ]);
+
     Route::get('/categories', [
         'uses' => 'CategoriesController@index',
         'as'   => 'categories'

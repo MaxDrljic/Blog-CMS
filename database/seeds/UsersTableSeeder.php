16cd36fd9909bd3f27c0d7a8876f59441a217ab6
MaxDrljic <max_spree@hotmail.com>
2018-12-27 09:42:38 +0100

    Admin, creating users

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,10 +11,19 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
-            'name' => 'max drljic',
-            'email' => 'max@test.com',
-            'password' => bcrypt('password')
+        $user = App\User::create([
+            'name'      => 'max drljic',
+            'email'     => 'max@test.com',
+            'password'  => bcrypt('password'),
+            'admin'     => 1
+        ]);
+
+        App\Profile::create([
+            'user_id'   => $user->id,
+            'avatar'    => 'uploads/avatars/1.png',
+            'about'     => 'Lorem ipsum dolor sit amet, est case denique ea, cu quem etiam sea. At est omnes tation, adhuc libris patrioque ea eum, cu corpora delectus inimicus qui.',
+            'facebook'  => 'facebook.com',
+            'youtube'   => 'youtube.com'
         ]);
     }
 }

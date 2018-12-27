13bce6fee0fb6c96794b0c7103605970e760fae7
MaxDrljic <max_spree@hotmail.com>
2018-12-27 09:51:52 +0100

    Admin and user permissions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -48,7 +48,8 @@ class UsersController extends Controller
         ]);
 
         $profile = Profile::create([
-            'user_id' => $user->id
+            'user_id'  => $user->id,
+            'avatar'   => 'uploads/avatars/1.png'
         ]);
 
         toastr()->success('User added successfully!');
@@ -99,5 +100,31 @@ class UsersController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function admin($id)
+    {
+        $user = User::find($id);
+
+        $user->admin = 1;
+
+        $user->save();
+
+        toastr()->success('Successfully saved user permissions!');
+
+        return redirect()->back();
+    }
+
+    public function not_admin($id)
+    {
+        $user = User::find($id);
+
+        $user->admin = 0;
+
+        $user->save();
+
+        toastr()->success('Successfully saved user permissions!');
+
+        return redirect()->back();
     }
 }

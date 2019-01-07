7305f0b1af48102ba8bcd40eb8a2ba6f6c79e7bc
MaxDrljic <max_spree@hotmail.com>
2019-01-07 08:46:22 +0100

    Included Disqus for comments

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Tag;
 use App\Post;
 use App\Category;
 use App\Setting;
@@ -34,6 +35,7 @@ class FrontEndController extends Controller
                              ->with('settings', Setting::first())
                              ->with('categories', Category::take(5)->get())
                              ->with('next', Post::find($next_id))
-                             ->with('prev', Post::find($prev_id));
+                             ->with('prev', Post::find($prev_id))
+                             ->with('tags', Tag::all());
     }
 }

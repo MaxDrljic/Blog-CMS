095791202cfa55363a20621fb60a61389a14d326
MaxDrljic <max_spree@hotmail.com>
2019-01-08 12:29:29 +0100

    AddThis share buttons

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -155,8 +155,10 @@
 <script src="{{ asset('app/js/ScrollMagic.min.js') }}"></script>
 <script src="{{ asset('app/js/animation.velocity.min.js') }}"></script>
 
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5c347d46d11963de"></script>
 
 <!-- ...end JS Script -->
+
 
 </body>
 </html>
